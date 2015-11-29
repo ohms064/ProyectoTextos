a501d3a56d195b6c68ea1a2c4c4d6a083ed8e88b
oms064 <otokonoko064@gmail.com>
2015-11-29 01:06:41 -0600

    Cambios en Limpiador.py. Actualicé la código para que funcione en conjunto con el parser y qué si detecta una palabra individual en un renglón no la escriba.

--- Limpiador.py
+++ Limpiador.py
@@ -11,20 +11,20 @@ import time
 START_TIME = time.time()
 pattern = "\s*<.*>\s*|\s*ENDOFARTICLE.\s*|[+*/=;,.:]" #Limpia el archivo
 
-lil_words = r"\W*\b\w{1,3}\b" #Este patrón también elimina palabras pequeñas
+lil_words = r"\W*\b\w{1,2}\b" #Este patrón también elimina palabras pequeñas
 
 for root, dirs, files in walk("raw.es"):
 	for num, archivo in enumerate(files):
 
 		direccion = str(root) + "\\" + str(archivo)
-		if not direccion.endswith("_LIMPIO"):
+		if not direccion.endswith("_PARSED") and not direccion.endswith("_LIMPIO"):
 			print("Limpiando: " + direccion)
 			with open(direccion, errors='ignore', encoding='utf8') as archivo:
 				with open(direccion + "_LIMPIO", "w", encoding='utf8') as salida:
 					for line in archivo:
 						line2 = re.sub(pattern, " ", line)
 						line2 = re.sub(lil_words, "", line2)
-						if line2.strip(): #Si la linea no está vacía
-							salida.write(line2)
+						if line2.strip() and len(line2.split()) > 1: #Si la linea no está vacía y contiene más de una palabra
+							salida.write(line2.lstrip())
 
 print("--- Total: {:05.2f} seconds ---".format((time.time() - START_TIME)))
