a8dde53881c49f8f14a94aa2aa21b0b3f882510b
oms064 <otokonoko064@gmail.com>
2015-12-03 13:42:36 -0600

    Junté limpiador en el Parser2 y ya se crean los indices unificados para todos los archivos.

--- Parser2.py
+++ Parser2.py
@@ -5,29 +5,45 @@ en una sóla linea.
 
 from os import walk
 import re
-import time #Sólo para contar el tiempo que tarda el programa
+def parser(subdir="raw.es", encode="utf8"):
+	"""
+		Función que convierte un archivo del corpus de wikicorpus a un archivo donde cada linea 
+		es un documento en lugar de separarlo en etiquetas. Se hace un filtrado para evitar
+		palabras individuales, palabras pequeñas y símbolos.
+		Entradas:
+			subdir: La carpeta donde se encuentran los archivos a parsear.
+			encode: La codificación que tienen los archivos que se quieren parsear. Se asume que todos
+			los archivos de la carpeta tienen la misma codificación.
+	"""
+	pattern = "\s*ENDOFARTICLE.\s*|\W" #Limpia el archivo
+	lil_words = r"\W*\b\w{1,3}\b" #Este patrón también elimina palabras pequeñas
+	init = ".*<doc.*>.*"
+	fin = ".*</doc>.*"
+	espacios = "\s+"
+	lineWrite = ""
+	for root, dirs, files in walk(subdir): #Obtiene la lista de los archivos
+		numFiles = len(files)
+		for num, archivo in enumerate(files): #Itera sobre la lista de los archivos
+			direccion = str(root) + "\\" + str(archivo)
+			direccionParsed = str(root) + "\\Parseados\\" + str(archivo) #Obtenemos la dirección, se asume que existe la carpeta Parseados dentro de raw.es
+			if "_PARSED" not in direccion:
+				print("Parsing: " + direccion)
+				print("\t {} de {}".format(num+1, numFiles))
+				with open(direccion, errors='ignore', encoding=encode) as archivo:
+					with open(direccionParsed + "_PARSED", "w", encoding=encode) as salida:
+						for line in archivo:
+							if re.match(init, line):
+								lineWrite = ""
+							elif re.match(fin, line) and len(lineWrite.split()) > 30:
+								salida.write(lineWrite.lstrip() + "\n")
+							else:
+								line = re.sub(pattern, " ", line)
+								line = re.sub(lil_words, "", line)
+								line = re.sub(espacios, " ", line)
+								if len(line.strip().split()) > 1:
+									lineWrite += " " + line.strip().lower()
+	print("Se terminó de hacer el parser")
 
-init = ".*<doc.*>.*"
-fin = ".*</doc>.*"
-ignorar = "\s*ENDOFARTICLE.\s*"
 
-START_TIME = time.time() 
-lineWrite = ""
-
-for root, dirs, files in walk("raw.es"): #Obtiene la lista de los archivos
-	for num, archivo in enumerate(files): #Itera sobre la lista de los archivos
-		direccion = str(root) + "\\Parseados\\" + str(archivo) #Obtenemos la dirección, se asume que existe la carpeta Parseados dentro de raw.es
-		if not "_PARSED" in direccion and direccion.endswith("_LIMPIO"):
-			print("Parsing: " + direccion)
-			with open(direccion, errors='ignore', encoding='utf8') as archivo:
-				with open(direccion + "_PARSED", "w", encoding='utf8') as salida:
-					for line in archivo:
-						if re.match(init, line):
-							lineWrite = ""
-						elif re.match(fin, line):
-							salida.write(lineWrite.lstrip() + "\n")
-						elif not re.match(ignorar, line):
-							lineWrite += " " + line.strip()
-
-
-print("--- Total: {:05.2f} seconds ---".format((time.time() - START_TIME)))
+if __name__ == "__main__":
+	parser(encode="cp1252")
--- index.py
+++ index.py
@@ -4,25 +4,40 @@ Se encarga indexar los tokens de un archivo y convertir los tokens a enteros
 
 from os import walk
 import json
-
-for root, subdir, files in walk("raw.es\\Parseados"):
-	for nombre in files:
-		print("Creando indices de: " + nombre)
-		vocabs = set()
-		direccion = root + "\\" + nombre
-		salida = "raw.es\\Indexados\\" + nombre
-		with open(direccion, errors="ignore", encoding="utf8") as archivo:
-			for line in archivo:
-				for word in line.strip().split():
-					vocabs.add(word)
-		decoding = dict(list(enumerate(vocabs)))
-		coding = dict([(y,x) for x,y in enumerate(vocabs)])
-		with open(salida +"_INDEXMAP", "w", encoding="utf8") as index:
-			json.dump(decoding, index)
-		with open(direccion, errors="ignore", encoding="utf8") as archivo:
-			with open(salida + "_INDEXED", "w") as indexed:
+def createIndexMap(subdir="raw.es\\Parseados", encode="utf8"):
+	vocabs = set()
+	for root, dirs, files in walk(subdir):
+		numFiles = len(files)
+		for num, nombre in enumerate(files):
+			print("Obteniendo indices de: " + nombre)
+			print("\t {} de {}".format(num+1, numFiles))
+			direccion = root + "\\" + nombre
+			salida = root.split("\\")[0] +  "\\Indexados\\" + nombre
+			with open(direccion, errors="ignore", encoding=encode) as archivo:
 				for line in archivo:
-					lineIndexed = ""
-					for word in line.split():
-						lineIndexed += str(coding[word]) + " "
-					indexed.write(lineIndexed + "\n")
+					for word in line.strip().split():
+						vocabs.add(word)
+	decoding = dict(list(enumerate(vocabs)))
+	coding = dict([(y,x) for x,y in enumerate(vocabs)])
+	with open(root + "\\indexmap.json", "w", encoding=encode) as index:
+		json.dump(decoding, index, indent=1)
+	print("¡Se terminó de crear el mapa de índices!")
+	return decoding, coding
+
+def createIndexFiles(subdir="raw.es\\Parseados", encode="utf8", coding=None):
+	for root, dirs, files in walk(subdir):
+		numFiles = len(files)
+		for num, nombre in enumerate(files):
+			print("Codificando: " + nombre)
+			print("\t {} de {}".format(num+1, numFiles))
+			with open(direccion, errors="ignore", encoding=encode) as archivo:
+				with open(salida + "_INDEXED", "w") as indexed:
+					for line in archivo:
+						lineIndexed = ""
+						for word in line.split():
+							lineIndexed += str(coding[word]) + " "
+						indexed.write(lineIndexed + "\n")
+
+if __name__ == "__main__":
+	decoding, code = createIndexMap(encode="cp1252")
+	createIndexFiles(encode="cp1252", coding=code)
