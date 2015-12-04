e77b6a6d561057e92129940a62de53cf6040572d
oms064 <otokonoko064@gmail.com>
2015-12-04 00:37:01 -0600

    Ya obtiene los títulos de cada documento y crea un archivo json con dichos títulos con el número de artículo como llave.

--- Parser2.py
+++ Parser2.py
@@ -5,6 +5,7 @@ en una sóla linea.
 
 from os import walk
 import re
+import json
 def parser(subdir="raw.es", encode="utf8"):
 	"""
 		Función que convierte un archivo del corpus de wikicorpus a un archivo donde cada linea 
@@ -19,29 +20,39 @@ def parser(subdir="raw.es", encode="utf8"):
 	lil_words = r"\W*\b\w{1,3}\b" #Este patrón también elimina palabras pequeñas
 	init = ".*<doc.*>.*"
 	fin = ".*</doc>.*"
+	titleregex = r'title=".*?"'
 	espacios = "\s+"
 	lineWrite = ""
 	for root, dirs, files in walk(subdir): #Obtiene la lista de los archivos
 		numFiles = len(files)
 		for num, archivo in enumerate(files): #Itera sobre la lista de los archivos
-			direccion = str(root) + "\\" + str(archivo)
-			direccionParsed = str(root) + "\\Parseados\\" + str(archivo) #Obtenemos la dirección, se asume que existe la carpeta Parseados dentro de raw.es
+			direccion = root + "\\" + archivo
+			direccionParsed = root + "\\Parseados\\" + archivo #Obtenemos la dirección, se asume que existe la carpeta Parseados dentro de raw.es
+			direccionTitles = root + "\\Titulos\\" + archivo + "_TITLE.json"
 			if "_PARSED" not in direccion:
+				titles = list()
 				print("Parsing: " + direccion)
 				print("\t {} de {}".format(num+1, numFiles))
 				with open(direccion, errors='ignore', encoding=encode) as archivo:
 					with open(direccionParsed + "_PARSED", "w", encoding=encode) as salida:
 						for line in archivo:
 							if re.match(init, line):
+								t = re.search(titleregex, line).group()
+								titles.append(re.sub(r'((title)|"|=)*', "", t))
 								lineWrite = ""
-							elif re.match(fin, line) and len(lineWrite.split()) > 30:
-								salida.write(lineWrite.lstrip() + "\n")
+							elif re.match(fin, line):
+								if len(lineWrite.split()) > 30:
+									salida.write(lineWrite.lstrip() + "\n")
+								else:
+									titles.pop()
 							else:
 								line = re.sub(pattern, " ", line)
 								line = re.sub(lil_words, "", line)
 								line = re.sub(espacios, " ", line)
 								if len(line.strip().split()) > 1:
 									lineWrite += " " + line.strip().lower()
+				with open(direccionTitles, "w", encoding=encode) as titulo:
+					json.dump(dict(list(enumerate(titles))), titulo, indent=1)
 		break #Sólo iterar sobre un nivel
 	print("Se terminó de hacer el parser")
 
